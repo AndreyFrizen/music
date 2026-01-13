d63d6ecaf2de9a0aeecbd12f46922e50fa9ad77e
andrey <fsfgc23d@gmail.com>
2026-01-13 21:38:46 +0300

    v1

--- internal/api/handlers/users.go
+++ internal/api/handlers/users.go
@@ -58,3 +58,46 @@ func (h *Handler) LoginUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "User logged in successfully", "token": token})
 }
+<<<<<<< HEAD
+=======
+
+// AuthMiddleware middleware for authentication
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+			c.Abort()
+			return
+		}
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		id, err := parseToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+		c.Set("userID", id)
+		c.Next()
+	}
+}
+
+func parseToken(tokenString string) (string, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &services.TokenClaims{}, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid method")
+		}
+		return secretKey, nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(*services.TokenClaims)
+	if !ok {
+		return "", errors.New("invalid claims")
+	}
+
+	return claims.UserID, nil
+}
+>>>>>>> ed7a845 (v1)
