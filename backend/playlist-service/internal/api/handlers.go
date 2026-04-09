d6d82a51a42a028626703737dee7328bce65f77b
Andrey A.A.. Frizen <frizenandrey@bkgau.ru>
2026-04-09 14:57:40 +0300

    v1

--- backend/playlist-service/internal/api/handlers.go
+++ backend/playlist-service/internal/api/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"log/slog"
+	"playlist-service/internal/domain/errors"
 	"playlist-service/internal/domain/model"
 	"playlist-service/proto/playlist"
 
@@ -79,4 +80,55 @@ func (s *serverAPI) DeletePlaylist(ctx context.Context, req *playlist.DeletePlay
 		return nil, err
 	}
 	return &playlist.DeletePlaylistResponse{Success: true}, nil
+}
+
+func (s *serverAPI) UpdatePlaylist(ctx context.Context, req *playlist.UpdatePlaylistRequest) (*playlist.UpdatePlaylistResponse, error) {
+	const op = "handlers.UpdatePlaylist"
+	s.log.With(slog.String("op", op))
+
+	userId, ok := ctx.Value("user_id").(int64)
+	if !ok {
+		s.log.ErrorContext(ctx, "failed to ", slog.String("op", op), slog.String("error", "user_id not found"))
+		return nil, errors.UnauthorizedError(op, "user unauthorized")
+	}
+
+	p, err := s.service.UpdatePlaylist(ctx, &model.Playlist{ID: req.Id, Title: req.Title, UserID: userId})
+	if err != nil {
+		s.log.ErrorContext(ctx, "failed to ", slog.String("op", op), slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	play := &playlist.Playlist{
+		Id:     p,
+		UserId: userId,
+		Title:  req.Title,
+	}
+
+	return &playlist.UpdatePlaylistResponse{Playlist: play}, nil
+}
+
+func (s *serverAPI) AddTrackToPlaylist(ctx context.Context, req *playlist.AddTrackRequest) (*playlist.AddTrackResponse, error) {
+	const op = "handlers.AddTrackToPlaylist"
+	s.log.With(slog.String("op", op))
+
+	p, err := s.service.AddTrackToPlaylist(ctx, &model.PlaylistTrack{PlaylistID: req.Id, TrackID: req.TrackId, Position: req.Position})
+	if err != nil {
+		s.log.ErrorContext(ctx, "failed to ", slog.String("op", op), slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	return &playlist.AddTrackResponse{Id: p}, nil
+}
+
+func (s *serverAPI) RemoveTrackFromPlaylist(ctx context.Context, req *playlist.RemoveTrackToPlaylistRequest) (*playlist.RemoveTrackToPlaylistResponse, error) {
+	const op = "handlers.RemoveTrackFromPlaylist"
+	s.log.With(slog.String("op", op))
+
+	p, err := s.service.RemoveTrackFromPlaylist(ctx, req.Id)
+	if err != nil {
+		s.log.ErrorContext(ctx, "failed to ", slog.String("op", op), slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	return &playlist.RemoveTrackToPlaylistResponse{Id: p}, nil
 }
